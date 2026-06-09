61ace62fba257bb80666ea71a997f6c9887d4f6a
Denuvo Cracker <hrai1729@gmail.com>
2026-06-10 01:11:27 +0530

    deploymet ready with gemini

--- server/utils/openaiClient.js
+++ server/utils/openaiClient.js
@@ -1,8 +1,13 @@
-const ollama = require('ollama').default;
 const { GoogleGenerativeAI } = require('@google/generative-ai');
+const { GoogleGenAI } = require('@google/genai');
+
 
 const genAI = new GoogleGenerativeAI(
   process.env.GEMINI_API_KEY
 );
 
-module.exports = { ollama, genAI };
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  });
+
+module.exports = { genAI, ai };
